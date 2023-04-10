e3752887e1aef78a0e2d780f35b28221f7de8b7c
João Carlos <joaocarlos@pickcells.bio>
2023-04-10 12:35:00 -0300

    create users to manage DB

---
+++ create_users.sql
@@ -0,0 +1,8 @@
+CREATE USER 'Aplicativo'@'localhost' IDENTIFIED BY '123456';
+GRANT INSERT, SELECT ON condominos.* TO 'Aplicativo'@'localhost';
+
+CREATE USER 'AdminGroup'@'localhost' IDENTIFIED BY '123456';
+GRANT INSERT, SELECT ON condominos.* TO 'AdminGroup'@'localhost';
+
+CREATE USER 'AdminBD'@'localhost' IDENTIFIED BY '123456';
+GRANT INSERT, SELECT ON condominos.* TO 'AdminBD'@'localhost';
