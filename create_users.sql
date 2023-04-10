bd23e587047b8a1adb3a9074b9afff3595ecc320
João Carlos <joaocarlos@pickcells.bio>
2023-04-10 12:36:06 -0300

    add flush command

--- create_users.sql
+++ create_users.sql
@@ -6,3 +6,5 @@ GRANT INSERT, SELECT ON condominos.* TO 'AdminGroup'@'localhost';
 
 CREATE USER 'AdminBD'@'localhost' IDENTIFIED BY '123456';
 GRANT INSERT, SELECT ON condominos.* TO 'AdminBD'@'localhost';
+
+FLUSH PRIVILEGES;
